36b4fcba5f89f76420a13acf3dca341ff23bf73f
Lunar <nk1s.skp>
2021-03-23 14:00:08 +0100

    Math and class setup

---
+++ ClickerApp/Buildings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClickerApp
+{
+    public class Building
+    {
+        public double amount { get; set; }
+        public double cost { get; set; }
+        public double value { get; set; }
+        public double upgrades { get; set; }
+    }
+}
