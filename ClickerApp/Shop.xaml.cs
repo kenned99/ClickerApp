62c82e89064bdc60df670f6aadbf3ad707af8efa
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-03-23 14:17:53 +0100

    Mainpage counter

---
+++ ClickerApp/Shop.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClickerApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Shop : ContentView
+    {
+        public Shop()
+        {
+            InitializeComponent();
+        }
+    }
+}
