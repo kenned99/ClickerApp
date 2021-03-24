7fed118663c83f3b33d85629870dad818ff3a216
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-03-24 15:10:04 +0100

    ListView with the right data

--- ClickerApp/Shop.xaml.cs
+++ ClickerApp/Shop.xaml.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,61 +12,76 @@ namespace ClickerApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Shop : ContentPage
     {
+        MainPage main = new MainPage();
         public Shop()
         {
             InitializeComponent();
             ScrollView scrollView = new ScrollView();
             scrollView.Scrolled += ScrollView_Scrolled;
             ShopListView.ItemsSource = itemlist.Items;
+            
         }
-
         private void Button_Clicked(object sender, EventArgs e)
         {
             
         }
-
+        
         private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
         {
             Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
         }
-    }
-    public static class itemlist{
-        public static IList<Item> Items { get; set; }
-    static itemlist()
+
+
+        List<Building> bList = new List<Building>();
+
+        private List<Building> BuildingList()
         {
-            Items = new ObservableCollection<Item>()
-            {
-                new Item
-                {
-                    name = "Katana",
-                    price = 10,
-                    amount = 0
-                },
-                new Item
-                {
-                    name = "Fedora",
-                    price = 100,
-                    amount = 0
-                },
-                new Item
-                {
-                    name = "Waifu",
-                    price = 1000,
-                    amount = 0
-                },
-                new Item
-                {
-                    name = "Bodypillow",
-                    price = 5000,
-                    amount = 0
-                },
-            };
+            bList.Add(main.katana);
+            bList.Add(main.fedora);
+            bList.Add(main.waifu);
+            bList.Add(main.bodypillow);
+            return bList;
         }
     }
-    public class Item
-    {
-        public string name { get; set; }
-        public int price { get; set; }
-        public int amount { get; set; }
+        public static class itemlist
+        {
+            public static IList<item> Items { get; set; }
+            static itemlist()
+            {
+                MainPage page = new MainPage();
+                Items = new ObservableCollection<item>()
+            {
+             new item{
+                    name = "Katana",
+                    cost = page.katana.cost,
+                    amount = page.katana.amount
+                },
+             new item{
+                    name = "Fedora",
+                    cost = page.fedora.cost,
+                    amount = page.fedora.amount
+                },
+             new item{
+                    name = "Waifu",
+                    cost = page.waifu.cost,
+                    amount = page.waifu.amount
+                },
+             new item{
+                    name = "Bodypillow",
+                    cost = page.bodypillow.cost,
+                    amount = page.bodypillow.amount
+                }
+            };
+
+            }
+        }
+
+        public class item
+        {
+            public string name { get; set; }
+            public double cost { get; set; }
+            public double amount { get; set; }
     }
-}
+
+    }
+   
