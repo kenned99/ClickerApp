78d26f961c3e55eff69ba9433e25ade1cd79e7bb
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-03-24 16:36:00 +0100

    All upgrade

--- ClickerApp/Shop.xaml.cs
+++ ClickerApp/Shop.xaml.cs
@@ -19,31 +19,37 @@ namespace ClickerApp
             ScrollView scrollView = new ScrollView();
             scrollView.Scrolled += ScrollView_Scrolled;
             ShopListView.ItemsSource = itemlist.Items;
-            
-        }
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            
         }
         
+
+
         private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
         {
             Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
         }
 
-
-        List<Building> bList = new List<Building>();
-
-        private List<Building> BuildingList()
+        private void Add(object sender, EventArgs e)
         {
-            bList.Add(main.katana);
-            bList.Add(main.fedora);
-            bList.Add(main.waifu);
-            bList.Add(main.bodypillow);
-            return bList;
+
+
         }
+
+
+        /* Do not work becuase the Listview need the name.
+          List<Building> bList = new List<Building>();
+
+         private List<Building> BuildingList()
+         {
+             bList.Add(main.katana);
+             bList.Add(main.fedora);
+             bList.Add(main.waifu);
+             bList.Add(main.bodypillow);
+             return bList;
+         }*/
     }
-        public static class itemlist
+
+
+    public static class itemlist
         {
             public static IList<item> Items { get; set; }
             static itemlist()
@@ -57,9 +63,19 @@ namespace ClickerApp
                     amount = page.katana.amount
                 },
              new item{
+                    name = "Katana Upgrade",
+                    cost = page.katana.cost,
+                    amount = page.katana.upgrades
+                },
+             new item{
                     name = "Fedora",
                     cost = page.fedora.cost,
                     amount = page.fedora.amount
+                },
+             new item{
+                    name = "Fedora Upgrade",
+                    cost = page.fedora.cost,
+                    amount = page.fedora.upgrades
                 },
              new item{
                     name = "Waifu",
@@ -67,10 +83,20 @@ namespace ClickerApp
                     amount = page.waifu.amount
                 },
              new item{
+                    name = "Waifu Upgrade",
+                    cost = page.waifu.cost,
+                    amount = page.waifu.upgrades
+                },
+             new item{
                     name = "Bodypillow",
                     cost = page.bodypillow.cost,
                     amount = page.bodypillow.amount
-                }
+                },
+             new item{
+                    name = "Bodypillow Upgrade",
+                    cost = page.waifu.cost,
+                    amount = page.waifu.upgrades
+                },
             };
 
             }
