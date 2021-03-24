d2b7a81f4fdc426feedd703fea3fe7edaf4d607b
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2021-03-24 12:10:59 +0100

    added saveobject and select user

--- Database/MongoDB.cs
+++ Database/MongoDB.cs
@@ -2,6 +2,7 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
 using System.Collections.Generic;
+using ClickerApp;
 
 
 
@@ -9,7 +10,7 @@ namespace Database
 {
     public class MongoDB
     {
-        static MongoClient cnn = new MongoClient("mongodb+srv://Kenned:Password123@cluster0.dg6zy.mongodb.net/mpc?authSource=admin&retryWrites=true&w=majority");
+        public static MongoClient cnn = new MongoClient("mongodb+srv://Kenned:Password123@cluster0.dg6zy.mongodb.net/mpc?authSource=admin&retryWrites=true&w=majority");
 
         public static MongoClient MongoDBConnect()
         {
@@ -22,21 +23,58 @@ namespace Database
 
             return cnn;
         }
+
+        /*public User Login(string email, string password)
+        {
+          //  User user = new User();
+        //  Nyt user objekt
+            var database = cnn.GetDatabase("mpc");
+            var collection = database.GetCollection<BsonDocument>("Users");
+            var filterEmail = Builders<BsonDocument>.Filter.Eq("email", email);
+            var filterPassword = Builders<BsonDocument>.Filter.Eq("password", password);
+            var data = collection.Find(filterEmail & filterPassword).FirstOrDefault();
+
+            return user;
+        }*/
+
+        public static Save SelectAnUser(string name, string key)
+        {
+            Save save = new Save();
+            var database = cnn.GetDatabase("mpc");
+            var collection = database.GetCollection<BsonDocument>("User");
+            var filter = Builders<BsonDocument>.Filter.Eq(key, name);
+
+            var data = collection.Find(filter).FirstOrDefault();
+            //user = UserObject(data);
+
+            return save;
+        }
+        public Save SaveObject(BsonDocument Tname)
+        {
+            Save save = new Save();
+
+            save.Username = (string)Tname["Username"];
+
+            save.katana = new Building()
+            {
+                amount = (double)Tname["Amount"],
+                cost = (double)Tname["Cost"],
+                upgrades = (double)Tname["Upgrades"],
+                value = (double)Tname["Value"]
+            };
+
+
+            save.fedora = new Building()
+            {
+                amount = (double)Tname["Amount"],
+                cost = (double)Tname["Cost"],
+                upgrades = (double)Tname["Upgrades"],
+                value = (double)Tname["Value"]
+            };
+
+            return save;
+        }
+
+
     }
-
-    /*public User Login(string email, string password)
-    {
-      //  User user = new User();
-    //  Nyt user objekt
-        var database = cnn.GetDatabase("mpc");
-        var collection = database.GetCollection<BsonDocument>("Users");
-        var filterEmail = Builders<BsonDocument>.Filter.Eq("email", email);
-        var filterPassword = Builders<BsonDocument>.Filter.Eq("password", password);
-        var data = collection.Find(filterEmail & filterPassword).FirstOrDefault();
-
-        return user;
-    }*/
-
-
-
 }
